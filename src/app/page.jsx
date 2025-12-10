bb4db39a798ffd6e527b0eaa54b9a2b73b0ef1cc
ferdouszihad <f.zihad333@gmail.com>
2025-12-10 16:06:27 +0600

    added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,12 +5,12 @@ export default function Home() {
     <div className="flex flex-col gap-5 min-h-screen justify-center text-center items-stretch">
       <div className="relative ">
         <h2 className="text-4xl font-bold">Next JS Fetching & Rendering</h2>
-        <h2 className="text-8xl absolute inset-0 bottom-0 opacity-10 font-medium">
-          Fetching & Rendering
+        <h2 className="text-8xl absolute inset-0 -top-20  opacity-10 font-medium">
+          Fetching Rendering Streaming Styling
         </h2>
       </div>
       <div className="">
-        <button className="btn">Let's Explore</button>
+        <button className="btn">Lets Explore</button>
       </div>
     </div>
   );
