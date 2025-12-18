e571169db793d0e95c6b69092e7e0cc251211f80
Asif-vai <mr3366533@gmail.com>
2025-12-18 16:34:08 +0600

    fix code for deploy

--- src/app/feedbacks/page.jsx
+++ src/app/feedbacks/page.jsx
@@ -1,28 +1,34 @@
+export const dynamic = 'force-dynamic'
+
 import FeedbackCard from '@/components/cards/FeedbackCard'
 import Link from 'next/link'
 import React from 'react'
 
-
-const getFeedbacks = async() =>{
-  const res = await fetch(`${process.env.NEXT_PUBLIC_server}/api/feedback`)
-  const data = await res.json()
-  return data
+const getFeedbacks = async () => {
+  const res = await fetch('/api/feedback', {
+    cache: 'no-store',
+  })
+  return res.json()
 }
 
 async function Feedbacks() {
-  const feedbacks = await getFeedbacks();
-  console.log(feedbacks)
+  const feedbacks = await getFeedbacks()
+
   return (
     <div>
-      <h1 className='text-4xl font-semibold my-6'>{feedbacks.length} Feed backs Found</h1>
-      <div>
-       <Link href='/addfeedback'> <button className='btn my-3'>Add Feed Back</button></Link>
-      </div>
+      <h1 className='text-4xl font-semibold my-6'>
+        {feedbacks.length} Feed backs Found
+      </h1>
+
+      <Link href='/addfeedback'>
+        <button className='btn my-3'>Add Feed Back</button>
+      </Link>
+
       <div className='flex flex-col gap-3'>
-      {
-        feedbacks.map(feedback =><FeedbackCard feedback={feedback} key={feedback._id}></FeedbackCard>)
-      }
-    </div>
+        {feedbacks.map(feedback => (
+          <FeedbackCard key={feedback._id} feedback={feedback} />
+        ))}
+      </div>
     </div>
   )
 }
